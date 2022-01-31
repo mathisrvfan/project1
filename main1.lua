b853bb148ae51486d9990a3d80165cf778321472
mathisrvfan <98718993+mathisrvfan@users.noreply.github.com>
2022-01-31 07:53:34 -0500

    main.lua

---
+++ main1.lua
@@ -0,0 +1,37 @@
+function sleep(n)
+    n = math.ceil(n)
+    if n <= 0 and n > 99999 then return end --If the user enter a number below 0 and higher than 99999 the limit in TIMEOUT command in Windows
+    os.execute("timeout /T "..tostring(seconds).." /NOBREAK")
+end
+local a = 1
+local b = 2
+local c = 345
+local d = 1670500
+local e = 1789709870
+local f = "10^50"
+local g = "10^10^10^10^10^10"
+local aa = "10^^^^^10"
+local ab = "10{25}10"
+print(a)
+sleep(5)
+print(b)
+sleep(3)
+print(c)
+sleep(5)
+print(d)
+sleep(10)
+print(g)
+sleep(10)
+print(ab)
+sleep(1)
+print("loop 1")
+sleep(1)
+print("loop 1.57")
+sleep(1)
+print("loop 10^5")
+sleep(2)
+print("loop 10^10^10^10^10^10^10^10^10")
+sleep(50)
+print("ultra gigachad loop infinite")
+sleep(500)
+print("ultra gigachad patient loop absolute end")
